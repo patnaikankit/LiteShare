d8eabbb69218361e1b892379f8d20d1ad4231ddf
Ankit Patnaik <116827173+patnaikankit@users.noreply.github.com>
2024-03-07 10:31:08 +0530

    chat functionality

---
+++ client/components/Chat.tsx
@@ -0,0 +1,112 @@
+// the logic for the chatting part is implemented
+import React, { useEffect, useRef, useState } from "react"
+import { Input } from "./UI/Input"
+import { Button } from "./UI/Button"
+import { SendHorizonal } from "lucide-react"
+import { useSocket } from "@/helper/SocketProvider"
+
+export const Chat = () => {
+    // States
+
+    // to store the messages until the connection lasts
+    const [message, setMessage] = useState<any[]>([]);
+    // to store a new message by any of the peer
+    const [newMessage, setNewMessage] = useState<any>("");
+    const inputRef = useRef<any>();
+    const buttonRef = useRef<any>();
+    const Socket = useSocket();
+
+
+    // Functions
+
+    // fucntion is triggered when a new message is ent
+    const handleMessage = () => {
+        if(newMessage.trim() !== ""){
+            const newMessages = [...message, { text: newMessage, sender: "me" }];
+            setMessage(newMessages);
+            setNewMessage("");
+
+            // to send the message to the peer
+            const peer = Socket.peerState;
+            if(peer){
+                const msgData = {
+                    type: "message",
+                    text: newMessage,
+                    sender: "other"
+                }
+                peer.send(JSON.stringify(msgData))
+            }
+        }
+    }
+
+    // eventlistener for incoming message
+    React.useEffect(() => {
+        const temp = (e: KeyboardEvent) => {
+            if(e.key === "k" && (e.metaKey || e.ctrlKey)){
+                e.preventDefault();
+                inputRef.current.focus();
+            }
+            else if(e.key === "Enter"){
+                buttonRef.current.click();
+            }
+        } 
+        document.addEventListener("keydown", temp);
+        return () => {
+            document.removeEventListener("keydown", temp)
+        }
+    }, []);
+
+    return (
+        <>
+            {/* render the chat ui if a peer is connected */}
+            {Socket.peerState ? (
+                <div className="flex justify-center sm:w-fit w-full">
+                    <div className="flex flex-col flex-wrap border rounded-md sm:min-w-[400px] min-w-[95%] min-h-[400px] p-2">
+                        <div className="flex-1 overflow-y-auto w-full">
+                            {message.map((msg, key) => (
+                                <div 
+                                key={key}
+                                className={`flex ${
+                                    msg.sender === "me" ? "justify-end" : "justify-start"
+                                  } mb-[2px]`}
+                                >
+                                <div
+                                    className={`flex flex-wrap sm:max-w-[200px] max-w-[150px] text-sm rounded-3xl px-3 py-1 ${
+                                        msg.sender === "me"
+                                          ? "bg-blue-500 text-white"
+                                          : "bg-zinc-700  text-white"
+                                      }`}
+                                ></div>
+                                </div>
+                            ))}
+                        </div>
+
+                        <div className="flex justify-between bottom-1 w-full space-x-1">
+                            <div className="flex w-full">
+                                <Input 
+                                    type="text"
+                                    value={newMessage}
+                                    onChange={(event) => {
+                                        setNewMessage(event.target.value)
+                                    }}
+                                    ref={inputRef}
+                                    placeholder={`Sender Message to ${Socket.userId}`}
+                                />
+                            </div>
+                            <div>
+                                <Button
+                                    variant={"outline"}
+                                    className="p-3"
+                                    onClick={handleMessage}
+                                    ref={buttonRef}
+                                >
+                                    <SendHorizonal size={14} />
+                                </Button>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            ) : null}
+        </>
+    )
+}
---
+++ client/helper/SocketProvider.tsx
@@ -0,0 +1,44 @@
+// creating a socket context provider to manage socket data across different components
+"use client"
+
+import { nanoid } from "nanoid"
+import React, {
+    createContext,
+    useContext,
+    useMemo,
+    useState
+} from "react"
+import { io } from "socket.io-client"
+import { Socket } from "socket.io-client/debug"
+
+
+const SocketContext = createContext<any>({});
+
+export const useSocket = () => {
+    const socket: { socket: Socket; userId: any, socketID: any, setSocketId: any, peerState: any, setPeerState: any } = useContext(SocketContext);
+    return socket;
+}
+
+// setting up the connection and providing the data to the children
+export const SocketProvider = ({ children }: { children: React.ReactNode }) => {
+    // to ensure that the socket connection is made only once 
+    const socket = useMemo(() => {
+        return io(String(process.env.SERVER_URL));
+    }, []);
+
+    // to check the status of the peer's connection
+    const [peerState, setPeerState] = useState<any>()
+    // to hold the value of the peer's socketID corrsponding to its uniqueID
+    const [socketID, setSocktID] = useState<any>(socket)
+    const userID = useMemo(() => {
+        // returns unique 10 character ID
+        return nanoid(10);
+    }, []);
+
+    return(
+        <SocketContext.Provider value = {{ socket, userID
+        , socketID, setSocktID, peerState, setPeerState }}>
+            { children }
+        </SocketContext.Provider>
+    ) 
+} 
