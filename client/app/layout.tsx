a540b982df48db4895f910ac73d1c29d1a657a28
Ankit Patnaik <116827173+patnaikankit@users.noreply.github.com>
2024-03-13 09:40:14 +0530

    fix 8

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -1,45 +1,49 @@
-import type { Metadata } from "next"
-import { Inter } from "next/font/google"
-import "./globals.css"
-import { ThemeProvider } from "@/components/ThemeProvider"
-import Navbar from "@/components/Navbar"
-import React from "react"
+// app/layout.tsx
 
-const inter = Inter({ subsets: ["latin"] });
+import React from 'react';
+import { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
+import { ThemeProvider } from '@/components/ThemeProvider';
+import Navbar from '@/components/Navbar';
+
+const inter = Inter({ subsets: ['latin'] });
 
 export const metaData: Metadata = {
-    title: "LiteShare",
-    description: 
-    "FileDrop: Swift P2P file sharing and real-time chat. Experience seamless, secure data exchange with our WebRTC-powered Next.js app. Join us for instant messaging and efficient file transfer in a modern, user-friendly environment",
-    authors: [
-        {
-            name: "Ankit Patnaik",
-            url: "https://github.com/patnaikankit"
-        }
-    ],
-    // icons{
-    //     icon: ""
-    // }
-}
+  title: 'LiteShare',
+  description:
+    'FileDrop: Swift P2P file sharing and real-time chat. Experience seamless, secure data exchange with our WebRTC-powered Next.js app. Join us for instant messaging and efficient file transfer in a modern, user-friendly environment',
+  authors: [
+    {
+      name: 'Ankit Patnaik',
+      url: 'https://github.com/patnaikankit',
+    },
+  ],
+  // icons{
+  //     icon: ""
+  // }
+};
 
 export default function RootLayout({
-    children,
+  children,
+  metaData, 
 }: {
-    children: React.ReactNode;
-}){
-    return (
-        <html lang="en">
-            <body className={inter.className}>
-                <ThemeProvider
-                    attribute="class"
-                    defaultTheme="dark"
-                    enableSystem
-                    disableTransitionOnChange
-                >
-                    <Navbar />
-                    {children}
-                </ThemeProvider>
-            </body>
-        </html>
-    );
+  children: React.ReactNode;
+  metaData: Metadata; // Define metaData type here
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="dark"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Navbar />
+          {children}
+        </ThemeProvider>
+      </body>
+    </html>
+  );
 }
