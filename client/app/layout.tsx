43bc0a910e3547eeb675ea9b2448d078b0e42073
Ankit Patnaik <116827173+patnaikankit@users.noreply.github.com>
2024-03-13 11:27:40 +0530

    fix 11

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -7,27 +7,11 @@ import Navbar from '@/components/Navbar';
 
 const inter = Inter({ subsets: ['latin'] });
 
-// export const metaData: Metadata = {
-//   title: 'LiteShare',
-//   description:
-//     'FileDrop: Swift P2P file sharing and real-time chat. Experience seamless, secure data exchange with our WebRTC-powered Next.js app. Join us for instant messaging and efficient file transfer in a modern, user-friendly environment',
-//   authors: [
-//     {
-//       name: 'Ankit Patnaik',
-//       url: 'https://github.com/patnaikankit',
-//     },
-//   ],
-//   // icons{
-//   //     icon: ""
-//   // }
-// };
-
 interface RootLayoutProps {
   metaData: Metadata;
-  children: React.ReactNode;
 }
 
-const RootLayout: React.FC<RootLayoutProps> = ({ children }) => {
+const RootLayout: React.FC<RootLayoutProps> = ({ metaData, children }) => {
   return (
     <html lang="en">
       <body className={inter.className}>
